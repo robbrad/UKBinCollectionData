4dc6818e9185f452eb8420a0b44bdc9eb3d18088
robbrad <robbrad182@gmail.com>
2021-05-15 12:15:23 +0100

    Moving to a template class - refactor of code

---
+++ scripts/get_bin_data.py
@@ -0,0 +1,34 @@
+import json
+from abc import ABC, abstractmethod
+
+#import the wonderful Beautiful Soup and the URL grabber
+import requests
+
+
+class AbstractGetBinDataClass(ABC):
+    def template_method(self,address_url) -> None:
+        page = self.get_data(address_url)
+        bin_data_dict = self.parse_data(page)
+        self.output_json(bin_data_dict)
+
+    def get_data(self, url) -> str:
+        #Set a user agent so we look like a browser ;-)
+        user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'
+        headers = {'User-Agent': user_agent}
+
+        #Make the Request - change the URL - find out your property number
+        full_page = requests.get(url,headers)
+
+        return full_page
+
+    @abstractmethod
+    def parse_data(self, page) -> dict:
+        pass
+
+    def output_json(self, bin_data_dict) -> str:
+        #Form a JSON wrapper
+        ##Make the JSON
+        json_data = json.dumps(bin_data_dict,sort_keys=True, indent=4)
+
+        #Output the data
+        print(json_data)
---
+++ scripts/run.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+#This script pulls (in one hit) the data from Cheshire East Council Bins Data
+import importlib
+import os
+import sys
+
+from bs4 import BeautifulSoup
+
+from get_bin_data import AbstractGetBinDataClass
+
+#from councils.CheshireEastCouncil import CheshireEastCouncil
+
+SRC_PATH = os.path.join('councils')
+module_path = os.path.realpath(
+    os.path.join(os.path.dirname(__file__),SRC_PATH)
+)
+sys.path.append(module_path)
+
+def client_code(get_bin_data_class: AbstractGetBinDataClass, address_url) -> None:
+    """
+    The client code calls the template method to execute the algorithm. Client
+    code does not have to know the concrete class of an object it works with, as
+    long as it works with objects through the interface of their base class.
+    """
+
+    get_bin_data_class.template_method(address_url)
+
+if __name__ == "__main__":
+    council_module_str = sys.argv[1]
+    address_url = sys.argv[2]
+    council_module = importlib.import_module(council_module_str)
+
+    client_code(council_module.CouncilClass(),address_url)
