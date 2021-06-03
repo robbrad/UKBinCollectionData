b8eeb35ece7dc0a3ef7d5be794bc7f2a7a02108b
robbrad <robbrad182@gmail.com>
2021-06-03 20:20:03 +0100

    Linting

--- scripts/councils/councilclasstemplate.py
+++ scripts/councils/councilclasstemplate.py
@@ -1,28 +1,31 @@
 #!/usr/bin/env python3
 
-#This script pulls (in one hit) the data from Warick District Council Bins Data
+# This script pulls (in one hit) the data from
+# Warick District Council Bins Data
 from bs4 import BeautifulSoup
-from get_bin_data import AbstractGetBinDataClass
+from ..get_bin_data import AbstractGetBinDataClass
 
-#import the wonderful Beautiful Soup and the URL grabber
+
+# import the wonderful Beautiful Soup and the URL grabber
 class CouncilClass(AbstractGetBinDataClass):
     """
-    Concrete classes have to implement all abstract operations of the base
-    class. They can also override some operations with a default implementation.
+    Concrete classes have to implement all abstract operations of the
+    base class. They can also override some operations with a default
+    implementation.
     """
 
     def parse_data(self, page) -> None:
-        #Make a BS4 object
+        # Make a BS4 object
         soup = BeautifulSoup(page.text, features="html.parser")
         soup.prettify()
 
-        data = {"bins":[]}
+        data = {"bins": []}
 
         for bins in soup.select('div[class*="service-item"]'):
             binType = bins.div.h3.text.strip()
-            binCollection = bins.select('div > p')[1].get_text(strip=True)
-            #binImage = "https://myaccount.stockport.gov.uk" + bins.img['src']
-            if binCollection: # batteries don't have a service date or other info associated with them.
+            binCollection = bins.select("div > p")[1].get_text(strip=True)
+            # binImage = "https://myaccount.stockport.gov.uk"   bins.img['src']
+            if (binCollection):
                 data[binType] = binCollection
 
         return data
