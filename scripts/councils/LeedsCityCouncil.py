e9e473eddd64fde2c36d10b31b1a50985d7994f4
David Park <dp247@rocketmail.com>
2022-07-24 18:32:28 +0100

    Replace user prompts with ValueErrors if invalid postcode or house numbers are provided. Script will then exit.

--- scripts/councils/LeedsCityCouncil.py
+++ scripts/councils/LeedsCityCouncil.py
@@ -25,13 +25,23 @@ class CouncilClass(AbstractGetBinDataClass):
         user_paon = kwargs.get("paon")
         postcode_re = "^([A-Za-z][A-Ha-hJ-Yj-y]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})$"
 
-        if user_postcode is None or not re.fullmatch(postcode_re, user_postcode):
-            print("Provide a postcode: ")
-            user_postcode = input("> ")
+        try:
+            if user_postcode is None or not re.fullmatch(postcode_re, user_postcode):
+                raise ValueError("Invalid postcode")
+        except Exception as ex:
+            print(f"Exception encountered: {ex}")
+            print("Please check the provided postcode. If this error continues, please first trying setting the "
+                  "postcode manually on line 24 before raising an issue.")
+            exit(1)
 
-        if user_paon is None:
-            print("Provide a house number: ")
-            user_paon = input("> ")
+        try:
+            if user_paon is None:
+                raise ValueError("Invalid house number")
+        except Exception as ex:
+            print(f"Exception encountered: {ex}")
+            print("Please check the provided house number. If this error continues, please first trying setting the "
+                  "house number manually on line 25 before raising an issue.")
+            exit(1)
 
         data = {"bins": []}  # dictionary for data
         prop_id = 0  # LCC use city wide URPNs in this dataset
