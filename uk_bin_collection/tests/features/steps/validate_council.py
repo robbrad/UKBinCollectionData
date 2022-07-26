a48be27086404a285dad36ce374554bef8a4f204
Robert Bradley <robbrad182@gmail.com>
2022-07-26 23:00:24 +0100

    #60 #53 #56 #64 final commit

--- uk_bin_collection/tests/features/steps/validate_council.py
+++ uk_bin_collection/tests/features/steps/validate_council.py
@@ -6,21 +6,24 @@ from uk_bin_collection.uk_bin_collection import collect_data
 @given('the council: "{council_name}"')
 def step_impl(context, council_name):
     council_input_data = file_handler.load_inputs_file("input.json")
-
     context.metadata = council_input_data[council_name]
-
     pass
-
 
 @when('we scrape the data from "{council}"')
 def step_impl(context, council):
-    print(context.metadata['url'])
+    context.council = council
     args = [council, context.metadata['url']]
-    parse_result = collect_data.main(args)
-    print(parse_result)
+    context.parse_result = collect_data.main(args)
     pass
 
-
-@then("behave will test it for us!")
+@then("the result is valid json")
 def step_impl(context):
+    valid_json = file_handler.validate_json(context.parse_result)
+    assert valid_json is True
+
+@then("the output should validate against the schema")
+def step_impl(context):
+    council_schema = file_handler.load_schema_file(f'{context.council}.schema')
+    file_handler.validate_json_schema(context.parse_result, council_schema)
     assert context.failed is False
+
--- uk_bin_collection/tests/features/validate_council_outputs.feature
+++ uk_bin_collection/tests/features/validate_council_outputs.feature
@@ -3,8 +3,8 @@ Feature: Test each council output matches expected results in /outputs
     Scenario Outline: Validate Council Output
         Given the council: "<council>"
         When we scrape the data from "<council>"
-        Then we should load the expected output
-        And the data output from the process should match
+        Then the result is valid json
+        And the output should validate against the schema
 
         Examples: Councils to Test
             | council      |
