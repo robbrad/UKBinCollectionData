8741b54e834fb418f1236747b5487b670d20e440
David Park <dp247@rocketmail.com>
2022-11-08 23:04:29 +0000

    Optimise imports

--- uk_bin_collection/uk_bin_collection/councils/SouthAyrshireCouncil.py
+++ uk_bin_collection/uk_bin_collection/councils/SouthAyrshireCouncil.py
@@ -1,10 +1,10 @@
-from bs4 import BeautifulSoup
-from uk_bin_collection.uk_bin_collection.common import *
-from uk_bin_collection.uk_bin_collection.get_bin_data import AbstractGetBinDataClass
-
-import requests
 import json
 from datetime import timedelta
+
+import requests
+
+from uk_bin_collection.uk_bin_collection.common import *
+from uk_bin_collection.uk_bin_collection.get_bin_data import AbstractGetBinDataClass
 
 
 # import the wonderful Beautiful Soup and the URL grabber
