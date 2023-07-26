c11e90e49f253b5c9f48e07cdc1e8340cdb8f44b
Robert Bradley <robbrad182@gmail.com>
2023-07-26 20:54:49 +0100

    more changes

--- custom_components/uk_bin_collection/__init__.py
+++ custom_components/uk_bin_collection/__init__.py
@@ -1,16 +1,19 @@
 """The UK Bin Collection Data integration."""
-import json
-from datetime import datetime
-
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.const import Platform
 from homeassistant.helpers.start import async_at_started
-from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
+from homeassistant.helpers import discovery
+from homeassistant.helpers.typing import (
+    DiscoveryInfoType,
+)
 
 # Make sure the 'uk_bin_collection' library is installed for this import to work
 from uk_bin_collection.uk_bin_collection.collect_data import UKBinCollectionApp
-from .const import DOMAIN, _LOGGER, LOG_PREFIX, PLATFORMS, SCAN_INTERVAL
+from .const import DOMAIN, LOG_PREFIX, PLATFORMS, SCAN_INTERVAL, CONF_COUNCIL, CONF_URL, CONF_SKIP_URL, CONF_UPRN, CONF_POSTCODE, CONF_HOUSE_NUMBER
 
+import logging
+_LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up UK Bin Collection Data from a config entry."""
@@ -18,20 +21,23 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     council_name = entry.data.get("council", "unknown council")
     _LOGGER.info(LOG_PREFIX + "Setting up UK Bin Collection Data for council: %s", council_name)
 
-    coordinator = UKBinCollectionDataUpdateCoordinator(hass, entry.data)
+    #coordinator = UKBinCollectionDataUpdateCoordinator(hass, entry.data)
 
     hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = coordinator
+    #hass.data[DOMAIN][entry.entry_id] = coordinator
 
     if entry.unique_id is None:
         name = entry.data["name"]
         hass.config_entries.async_update_entry(entry, unique_id=f"{name}")
 
+    _LOGGER.info(LOG_PREFIX + "Config entry data: %s", entry.data)
+
+
     async def _async_finish_startup(_):
-        await coordinator.async_refresh()
         await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     async_at_started(hass, _async_finish_startup)
+
     _LOGGER.info(LOG_PREFIX + "Successfully set up UK Bin Collection Data for council: %s", council_name)
     return True
 
@@ -40,49 +46,3 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
     return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
 
-
-class UKBinCollectionDataUpdateCoordinator(DataUpdateCoordinator[object | None]):
-    """Class to manage fetching UK Bin Collection Data council data."""
-
-    def __init__(self, hass, data):
-        """Initialize global UK Bin Collection Data updater."""
-        self._hass = hass
-        self._data = data
-        self.name = data["name"]
-        self.args = [
-            self._data.get("council", ""),
-            self._data.get("url", ""),
-            *(f"--{key}={value}" for key, value in self._data.items() if
-              key not in {"name", "council", "url", "skip_get_url"}),
-        ]
-        if "skip_get_url" in self._data:
-            self.args.append("--skip_get_url")
-        self.ukbcd = UKBinCollectionApp()
-        self.ukbcd.set_args(self.args)
-
-        super().__init__(
-            hass,
-            _LOGGER,
-            name=f"{DOMAIN}_{self.name}",
-            update_interval=SCAN_INTERVAL
-        )
-
-    async def _async_update_data(self) -> object | None:
-        """Fetch council data."""
-        try:
-            _LOGGER.info(LOG_PREFIX + "Collecting data for council: %s", self._data["council"])
-            # Run UKBCD and get the JSON string
-            json_string = await self._hass.async_add_executor_job(self.ukbcd.run)
-
-            # Parse the JSON string into a Python dictionary
-            council_data = json.loads(json_string)
-
-            # Sort data by collection date
-            council_data["bins"].sort(
-                key=lambda x: datetime.strptime(x.get("collectionDate"), "%d/%m/%Y")
-            )
-            _LOGGER.info(LOG_PREFIX + "Council Data: %s", council_data)
-            return council_data
-        except Exception as err:
-            _LOGGER.error(LOG_PREFIX + "Failed to collect data for council: %s", self._data["council"], exc_info=True)
-            return None
--- custom_components/uk_bin_collection/const.py
+++ custom_components/uk_bin_collection/const.py
@@ -1,5 +1,4 @@
 """Constants for UK Bin Collection Data."""
-import logging
 from datetime import timedelta
 
 from homeassistant.const import Platform
@@ -8,10 +7,21 @@ DEFAULT_NAME = "UK Bin Collection Data"
 
 DOMAIN = "uk_bin_collection"
 
-_LOGGER = logging.getLogger(__name__)
-
 LOG_PREFIX = "[UKBinCollection] "
 
 PLATFORMS = [Platform.SENSOR]
 
 SCAN_INTERVAL = timedelta(hours=24)
+
+STATE_ATTR_COLOUR = "colour"
+STATE_ATTR_NEXT_COLLECTION = "next_collection"
+STATE_ATTR_DAYS = "days"
+
+DEVICE_CLASS = "bin_collection_schedule"
+
+CONF_COUNCIL = "council"
+CONF_URL = "url"
+CONF_SKIP_URL = "skip_url"
+CONF_UPRN = "uprn"
+CONF_POSTCODE = "postcode"
+CONF_HOUSE_NUMBER = "house_number"
