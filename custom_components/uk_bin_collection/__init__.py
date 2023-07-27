ebcc7564b609554fa6fb6b327a3f3b7ccc2afa3a
Robert Bradley <robbrad182@gmail.com>
2023-07-27 07:57:15 +0100

    Formatting and Sensor improvements

--- custom_components/uk_bin_collection/__init__.py
+++ custom_components/uk_bin_collection/__init__.py
@@ -1,30 +1,32 @@
 """The UK Bin Collection Data integration."""
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.const import Platform
 from homeassistant.helpers.start import async_at_started
-from homeassistant.helpers import discovery
-from homeassistant.helpers.typing import (
-    DiscoveryInfoType,
-)
 
 # Make sure the 'uk_bin_collection' library is installed for this import to work
-from uk_bin_collection.uk_bin_collection.collect_data import UKBinCollectionApp
-from .const import DOMAIN, LOG_PREFIX, PLATFORMS, SCAN_INTERVAL, CONF_COUNCIL, CONF_URL, CONF_SKIP_URL, CONF_UPRN, CONF_POSTCODE, CONF_HOUSE_NUMBER
+from .const import (
+    DOMAIN,
+    LOG_PREFIX,
+    PLATFORMS,
+)
 
 import logging
+
 _LOGGER = logging.getLogger(__name__)
+
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up UK Bin Collection Data from a config entry."""
     _LOGGER.info(LOG_PREFIX + "Data Supplied: %s", entry.data)
     council_name = entry.data.get("council", "unknown council")
-    _LOGGER.info(LOG_PREFIX + "Setting up UK Bin Collection Data for council: %s", council_name)
+    _LOGGER.info(
+        LOG_PREFIX + "Setting up UK Bin Collection Data for council: %s", council_name
+    )
 
-    #coordinator = UKBinCollectionDataUpdateCoordinator(hass, entry.data)
+    # coordinator = UKBinCollectionDataUpdateCoordinator(hass, entry.data)
 
     hass.data.setdefault(DOMAIN, {})
-    #hass.data[DOMAIN][entry.entry_id] = coordinator
+    # hass.data[DOMAIN][entry.entry_id] = coordinator
 
     if entry.unique_id is None:
         name = entry.data["name"]
@@ -32,17 +34,18 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     _LOGGER.info(LOG_PREFIX + "Config entry data: %s", entry.data)
 
-
     async def _async_finish_startup(_):
         await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     async_at_started(hass, _async_finish_startup)
 
-    _LOGGER.info(LOG_PREFIX + "Successfully set up UK Bin Collection Data for council: %s", council_name)
+    _LOGGER.info(
+        LOG_PREFIX + "Successfully set up UK Bin Collection Data for council: %s",
+        council_name,
+    )
     return True
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
     return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
-
