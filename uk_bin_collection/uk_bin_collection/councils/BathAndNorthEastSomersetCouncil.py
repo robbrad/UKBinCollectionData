23a2d08bd988f3b4a33031ad8d845da722e334c2
David Park <dp247@rocketmail.com>
2023-10-13 11:54:28 +0100

    Resolve 'unsafe legacy renegotiation disabled' SSL error

--- uk_bin_collection/uk_bin_collection/councils/BathAndNorthEastSomersetCouncil.py
+++ uk_bin_collection/uk_bin_collection/councils/BathAndNorthEastSomersetCouncil.py
@@ -4,7 +4,20 @@ from bs4 import BeautifulSoup
 from uk_bin_collection.uk_bin_collection.common import *
 from uk_bin_collection.uk_bin_collection.get_bin_data import \
     AbstractGetBinDataClass
+import ssl
+import urllib3
 
+class CustomHttpAdapter (requests.adapters.HTTPAdapter):
+    '''Transport adapter" that allows us to use custom ssl_context.'''
+
+    def __init__(self, ssl_context=None, **kwargs):
+        self.ssl_context = ssl_context
+        super().__init__(**kwargs)
+
+    def init_poolmanager(self, connections, maxsize, block=False):
+        self.poolmanager = urllib3.poolmanager.PoolManager(
+            num_pools=connections, maxsize=maxsize,
+            block=block, ssl_context=self.ssl_context)
 
 class CouncilClass(AbstractGetBinDataClass):
     """
@@ -18,25 +31,29 @@ class CouncilClass(AbstractGetBinDataClass):
         check_uprn(user_uprn)
 
         headers = {
-            "accept": "application/json, text/javascript, */*; q=0.01",
-            "accept-encoding": "gzip, deflate, br",
-            "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
-            "connection": "keep-alive",
-            "content-type": "application/json",
-            "host": "www.bathnes.gov.uk",
-            "referer": "https://www.bathnes.gov.uk/webforms/waste/collectionday/",
-            "sec-fetch-dest": "empty",
-            "sec-fetch-mode": "cors",
-            "sec-fetch-site": "same-origin",
-            "sec-gpc": "1",
-            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36",
-            "x-requested-with": "XMLHttpRequest",
+            'Accept': 'application/json, text/javascript, */*; q=0.01',
+            'Accept-Language': 'en-GB,en;q=0.9',
+            'Cache-Control': 'no-cache',
+            'Connection': 'keep-alive',
+            'Content-Type': 'application/json; charset=utf-8',
+            'Pragma': 'no-cache',
+            'Referer': 'https://www.bathnes.gov.uk/webforms/waste/collectionday/',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-origin',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.5845.188 Safari/537.36',
+            'X-Requested-With': 'XMLHttpRequest',
         }
 
+        session = requests.Session()
+        ctx = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+        ctx.options |= 0x4
+        session.mount('https://', CustomHttpAdapter(ctx))
+
         requests.packages.urllib3.disable_warnings()
-        response = requests.get(
+        response = session.get(
             f"https://www.bathnes.gov.uk/webapi/api/BinsAPI/v2/getbartecroute/{user_uprn}/true",
-            headers=headers,
+            headers=headers
         )
         if response.text == "":
             raise ValueError("Error parsing data. Please check the provided UPRN. "
