4f0e13e073d37f56bf504b4715114600435efc91
David Park <dp247@rocketmail.com>
2023-11-29 11:51:49 +0000

    fix: change logic to add correct years and support 'Tomorrow' results

--- uk_bin_collection/uk_bin_collection/councils/CharnwoodBoroughCouncil.py
+++ uk_bin_collection/uk_bin_collection/councils/CharnwoodBoroughCouncil.py
@@ -3,6 +3,9 @@ from uk_bin_collection.uk_bin_collection.common import *
 from uk_bin_collection.uk_bin_collection.get_bin_data import \
     AbstractGetBinDataClass
 
+from datetime import timedelta
+from dateutil.relativedelta import relativedelta
+
 
 # import the wonderful Beautiful Soup and the URL grabber
 class CouncilClass(AbstractGetBinDataClass):
@@ -18,6 +21,7 @@ class CouncilClass(AbstractGetBinDataClass):
         soup.prettify()
 
         data = {"bins": []}
+        curr_date = datetime.today()
 
         for bins in soup.find_all("ul", {"class": "refuse"}):
             binCollection = bins.find_all("li")
@@ -27,11 +31,13 @@ class CouncilClass(AbstractGetBinDataClass):
                     collection_date = bin.find("strong", {"class": "date"}).contents[0].strip()
                     if collection_date.lower() == "today":
                         collection_date = datetime.now()
+                    elif collection_date.lower() == "tomorrow":
+                        collection_date = datetime.now() + timedelta(days=1)
                     else:
-                        collection_date = datetime.strptime(
-                            remove_ordinal_indicator_from_date_string(collection_date).strip(),
-                            "%a %d %b"
-                        )
+                        collection_date = datetime.strptime(remove_ordinal_indicator_from_date_string(collection_date)
+                                                            .strip(),"%a %d %b").replace(year=curr_date.year)
+                        if curr_date.month == 12 and collection_date.month == 1:
+                            bin_date = bin_date + relativedelta(years=1)
                     dict_data = {
                         "type": bin.find("a").contents[0],
                         "collectionDate": collection_date.strftime(date_format)
