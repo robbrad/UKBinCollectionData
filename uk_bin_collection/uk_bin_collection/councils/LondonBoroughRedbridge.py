27e8cf6307ddba57c65f454d1447b3168bb769e6
robbrad <warMojqyfxagruvmu5>
2023-12-30 22:14:54 +0000

    feat: Adding London Borough Redbridge

--- uk_bin_collection/uk_bin_collection/councils/LondonBoroughRedbridge.py
+++ uk_bin_collection/uk_bin_collection/councils/LondonBoroughRedbridge.py
@@ -48,56 +48,73 @@ class CouncilClass(AbstractGetBinDataClass):
 
         submit_btn.send_keys(Keys.ENTER)
 
-        address_results = wait.until(
-            EC.presence_of_element_located((By.CLASS_NAME, 'directories-table'))
-        )
         address_link = wait.until(
-            EC.presence_of_element_located((By.XPATH, f"//a[contains(text(), '{user_paon}')]"))
+            EC.presence_of_element_located((By.XPATH, f'//a[contains(@data-uprn,"{uprn}")]'))
         )
 
         address_link.send_keys(Keys.ENTER)
-        results = wait.until(
-            EC.presence_of_element_located((By.CLASS_NAME, "usercontent"))
+
+        address_results = wait.until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'your-collection-schedule-container'))
         )
+
 
         # Make a BS4 object
         soup = BeautifulSoup(driver.page_source, features="html.parser")
-        soup.prettify()
+        data = {"bins": []}
 
-        # Get collection calendar
-        calendar_urls = soup.find_all("a", string=re.compile(r"view or download the collection calendar"))
-        if len(calendar_urls) > 0:
-            requests.packages.urllib3.disable_warnings()
-            response = requests.get(calendar_urls[0].get("href"), headers=headers)
+        # Get the current month and year
+        current_month = datetime.now().month
+        current_year = datetime.now().year
 
-            # Make a BS4 object
-            soup = BeautifulSoup(response.text, features="html.parser")
-            soup.prettify()
+        # Function to extract collection data
+        def extract_collection_data(collection_div, collection_type):
+            if collection_div:
+                date_element = collection_div.find(class_='recycling-collection-day-numeric') or collection_div.find(
+                    class_='refuse-collection-day-numeric') or collection_div.find(class_='garden-collection-day-numeric')
+                month_element = collection_div.find(class_='recycling-collection-month') or collection_div.find(
+                    class_='refuse-collection-month') or collection_div.find(class_='garden-collection-month')
 
-            # Loop the months
-            for month in soup.find_all("div", {"class": "usercontent"}):
-                year = ""
-                if month.find("h2") and 'calendar' not in month.find("h2").get_text(strip=True):
-                    year = datetime.strptime(month.find("h2").get_text(strip=True), "%B %Y").strftime("%Y")
-                elif month.find("h3"):
-                    year = datetime.strptime(month.find("h3").get_text(strip=True), "%B %Y").strftime("%Y")
-                if year != "":
-                    for row in month.find_all("li"):
-                        results = re.search(
-                            "([A-Za-z]+ \\d\\d? [A-Za-z]+): (.+)", row.get_text(strip=True)
-                        )
-                        if results:
-                            dict_data = {
-                                "type": results.groups()[1].capitalize(),
-                                "collectionDate": datetime.strptime(
-                                    results.groups()[0] + " " + year, "%A %d %B %Y"
-                                ).strftime(date_format),
-                            }
-                            data["bins"].append(dict_data)
+                if date_element and month_element:
+                    collection_date = date_element.get_text(strip=True)
+                    collection_month = month_element.get_text(strip=True)
 
-            # Sort collections
-            data["bins"].sort(
-                key=lambda x: datetime.strptime(x.get("collectionDate"), "%d/%m/%Y")
-            )
+                    # Combine month, date, and year into a string
+                    date_string = f"{collection_date} {collection_month} {current_year}"
 
+                    try:
+                        # Convert the date string to a datetime object
+                        collection_date_obj = datetime.strptime(date_string, '%d %B %Y')
+
+                        # Check if the month is ahead of the current month
+                        if collection_date_obj.month >= current_month:
+                            # If the month is ahead, use the current year
+                            formatted_date = collection_date_obj.strftime(date_format)
+                        else:
+                            # If the month is before the current month, use the next year
+                            formatted_date = collection_date_obj.replace(year=current_year + 1).strftime(date_format)
+                        # Create a dictionary for each collection entry
+                        dict_data = {
+                            "type": collection_type,
+                            "collectionDate": formatted_date
+                        }
+
+                        # Append dictionary data to the 'bins' list in the 'data' dictionary
+                        data["bins"].append(dict_data)
+
+                    except ValueError as e:
+                        # Handle the case where the date format is invalid
+                        formatted_date = "Invalid Date Format"
+
+        # Extract Recycling collection data
+        extract_collection_data(soup.find(class_='container-fluid RegularCollectionDay').find_next_sibling('div'), "Recycling")
+
+        # Extract Refuse collection data
+        for refuse_div in soup.find_all(class_='container-fluid RegularCollectionDay'):
+            extract_collection_data(refuse_div, "Refuse")
+
+        # Extract Garden Waste collection data
+        extract_collection_data(soup.find(class_='container-fluid gardenwasteCollectionDay'), "Garden Waste")
+
+        # Print the extracted data
         return data
