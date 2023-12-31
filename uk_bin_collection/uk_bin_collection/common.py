aa51054935f98da49309d46dac7a8a7af7cf20bd
robbrad <warMojqyfxagruvmu5>
2023-12-31 08:39:45 +0000

    feat: Adding Brighton and Hove City Council

--- uk_bin_collection/uk_bin_collection/common.py
+++ uk_bin_collection/uk_bin_collection/common.py
@@ -249,7 +249,7 @@ def create_webdriver(web_driver) -> webdriver.Chrome:
     """
     # Set up Selenium to run 'headless'
     options = webdriver.ChromeOptions()
-    #options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-gpu")
     options.add_argument("--start-maximized")
