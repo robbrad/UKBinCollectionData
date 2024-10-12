364a64c36149935767bf7dfc159d679ca27fa1ed
m26dvd <31007572+m26dvd@users.noreply.github.com>
2024-10-13 00:15:35 +0100

    feat: Adding Lichfield District Council
    fix: #830

---
+++ uk_bin_collection/uk_bin_collection/councils/LichfieldDistrictCouncil.py
@@ -0,0 +1,60 @@
+import re
+
+import requests
+from bs4 import BeautifulSoup
+
+from uk_bin_collection.uk_bin_collection.common import *
+from uk_bin_collection.uk_bin_collection.get_bin_data import AbstractGetBinDataClass
+
+
+# import the wonderful Beautiful Soup and the URL grabber
+class CouncilClass(AbstractGetBinDataClass):
+    """
+    Concrete classes have to implement all abstract operations of the
+    base class. They can also override some operations with a default
+    implementation.
+    """
+
+    def parse_data(self, page: str, **kwargs) -> dict:
+
+        user_uprn = kwargs.get("uprn")
+        check_uprn(user_uprn)
+        bindata = {"bins": []}
+
+        def solve(s):
+            return re.sub(r"(\d)(st|nd|rd|th)", r"\1", s)
+
+        URI = f"https://www.lichfielddc.gov.uk/homepage/6/bin-collection-dates?uprn={user_uprn}"
+
+        # Make the GET request
+        response = requests.get(URI)
+
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        bins = soup.find_all("h3", class_="bin-collection-tasks__heading")
+        dates = soup.find_all("p", class_="bin-collection-tasks__date")
+
+        current_year = datetime.now().year
+        current_month = datetime.now().month
+
+        for i in range(len(dates)):
+            bint = " ".join(bins[i].text.split()[2:4])
+            date = dates[i].text
+
+            date = datetime.strptime(
+                solve(date),
+                "%d %B",
+            )
+
+            if (current_month > 10) and (date.month < 3):
+                date = date.replace(year=(current_year + 1))
+            else:
+                date = date.replace(year=current_year)
+
+            dict_data = {
+                "type": bint,
+                "collectionDate": date.strftime("%d/%m/%Y"),
+            }
+            bindata["bins"].append(dict_data)
+
+        return bindata
