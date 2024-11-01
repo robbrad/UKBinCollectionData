36e702c008da1ca5f9fd5bcdb3724b79e7d384e9
Robert Bradley <robbrad182@gmail.com>
2024-11-01 23:15:56 +0000

    feat: pytest fixes

--- uk_bin_collection/uk_bin_collection/councils/CheshireWestAndChesterCouncil.py
+++ uk_bin_collection/uk_bin_collection/councils/CheshireWestAndChesterCouncil.py
@@ -15,7 +15,6 @@ logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
-
 class CouncilClass(AbstractGetBinDataClass):
     def parse_data(self, page: str, **kwargs) -> dict:
         driver = None
