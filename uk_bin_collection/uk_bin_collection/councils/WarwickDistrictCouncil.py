3e2984a28b6d88839c409fe634d6d6b2d0afc9a5
m26dvd <31007572+m26dvd@users.noreply.github.com>
2024-11-27 21:48:01 +0000

    fix: Warwick District Council

--- uk_bin_collection/uk_bin_collection/councils/WarwickDistrictCouncil.py
+++ uk_bin_collection/uk_bin_collection/councils/WarwickDistrictCouncil.py
@@ -1,7 +1,11 @@
 # This script pulls (in one hit) the data
 # from Warick District Council Bins Data
 
+from datetime import datetime
+
 from bs4 import BeautifulSoup
+
+from uk_bin_collection.uk_bin_collection.common import *
 from uk_bin_collection.uk_bin_collection.get_bin_data import AbstractGetBinDataClass
 
 
@@ -20,15 +24,30 @@ class CouncilClass(AbstractGetBinDataClass):
 
         data = {"bins": []}
 
-        for element in soup.find_all("strong"):
-            bin_type = element.next_element
-            bin_type = bin_type.lstrip()
-            collectionDateElement = element.next_sibling.next_element.next_element
-            collectionDate = collectionDateElement.getText()
-            dict_data = {
-                "type": bin_type,
-                "collectionDate": collectionDate,
-            }
-            data["bins"].append(dict_data)
+        # Find all bin panels
+        bin_panels = soup.find_all("div", class_="col-sm-4 col-lg-3")
+
+        # Iterate through each panel to extract information
+        for panel in bin_panels:
+            bin_type = panel.find("img")["alt"].strip()
+
+            waste_dates = panel.find(
+                "div", class_="col-xs-12 text-center waste-dates margin-bottom-15"
+            )
+
+            for p in waste_dates.find_all("p")[1:]:
+                date = p.text.strip()
+                if " " in date:
+                    date = date.split(" ")[1]
+
+                dict_data = {
+                    "type": bin_type,
+                    "collectionDate": date,
+                }
+                data["bins"].append(dict_data)
+
+        data["bins"].sort(
+            key=lambda x: datetime.strptime(x.get("collectionDate"), date_format)
+        )
 
         return data
