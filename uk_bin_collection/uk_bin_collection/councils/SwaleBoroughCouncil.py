f057564f8705ddaed1d119e6127bb591c65791b0
m26dvd <31007572+m26dvd@users.noreply.github.com>
2024-12-12 23:38:06 +0000

    fix: Swale Borough Council
    fix: #1080
    (cherry picked from commit 6f580b39fb68b8079990221e050ae8dd6d2b7285)

--- uk_bin_collection/uk_bin_collection/councils/SwaleBoroughCouncil.py
+++ uk_bin_collection/uk_bin_collection/councils/SwaleBoroughCouncil.py
@@ -26,7 +26,7 @@ class CouncilClass(AbstractGetBinDataClass):
         check_paon(user_paon)
 
         # Build URL to parse
-        council_url = "https://swale.gov.uk/bins-littering-and-the-environment/bins/my-collection-day"
+        council_url = "https://swale.gov.uk/bins-littering-and-the-environment/bins/check-your-bin-day"
 
         # Create Selenium webdriver
         driver = create_webdriver(web_driver, headless, None, __name__)
@@ -35,7 +35,7 @@ class CouncilClass(AbstractGetBinDataClass):
         # Wait for the postcode field to appear then populate it
         try:
             inputElement_postcode = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.ID, "q462406_q1"))
+                EC.presence_of_element_located((By.ID, "q485476_q1"))
             )
             inputElement_postcode.send_keys(user_postcode)
         except Exception:
@@ -43,7 +43,7 @@ class CouncilClass(AbstractGetBinDataClass):
 
         # Click search button
         findAddress = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.ID, "form_email_462397_submit"))
+            EC.presence_of_element_located((By.ID, "form_email_485465_submit"))
         )
         driver.execute_script("arguments[0].click();", findAddress)
 
@@ -52,7 +52,7 @@ class CouncilClass(AbstractGetBinDataClass):
             EC.element_to_be_clickable(
                 (
                     By.XPATH,
-                    "//select[@id='SBCYBDAddressList']//option[contains(., '"
+                    "//select[@name='q485480:q1']//option[contains(., '"
                     + user_paon
                     + "')]",
                 )
@@ -61,12 +61,12 @@ class CouncilClass(AbstractGetBinDataClass):
 
         # Click search button
         getBins = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.ID, "form_email_462397_submit"))
+            EC.presence_of_element_located((By.ID, "form_email_485465_submit"))
         )
         driver.execute_script("arguments[0].click();", getBins)
 
         BinTable = WebDriverWait(driver, 30).until(
-            EC.presence_of_element_located((By.ID, "SBC-YBD-Main"))
+            EC.presence_of_element_located((By.ID, "SBCYBDSummary"))
         )
 
         soup = BeautifulSoup(driver.page_source, features="html.parser")
@@ -74,17 +74,41 @@ class CouncilClass(AbstractGetBinDataClass):
 
         data = {"bins": []}
 
-        # Get the collection bullet points on the page and parse them
-        nextCollections = soup.find("div", {"id": "nextCollections"})
-        for c in nextCollections:
-            collection = c.find_all("strong")
-            for bin in collection:
-                split = (bin.text).split(" on ")
-                bin_type = split[0]
-                bin_date = datetime.strptime(split[1], "%A %d %b %Y").strftime(
-                    "%d/%m/%Y"
-                )
-                dict_data = {"type": bin_type, "collectionDate": bin_date}
-                data["bins"].append(dict_data)
+        next_collection_date = soup.find(
+            "strong", id="SBC-YBD-collectionDate"
+        ).text.strip()
+
+        # Extract bins for the next collection
+        next_bins = [li.text.strip() for li in soup.select("#SBCFirstBins ul li")]
+
+        # Extract future collection details
+        future_collection_date_tag = soup.find(
+            "p", text=lambda t: t and "starting from" in t
+        )
+        future_collection_date = (
+            future_collection_date_tag.text.split("starting from")[-1].strip()
+            if future_collection_date_tag
+            else "No future date found"
+        )
+
+        future_bins = [li.text.strip() for li in soup.select("#FirstFutureBins li")]
+
+        for bin in next_bins:
+            dict_data = {
+                "type": bin,
+                "collectionDate": datetime.strptime(
+                    next_collection_date, "%A, %d %B"
+                ).strftime(date_format),
+            }
+            data["bins"].append(dict_data)
+
+        for bin in future_bins:
+            dict_data = {
+                "type": bin,
+                "collectionDate": datetime.strptime(
+                    future_collection_date, "%A, %d %B"
+                ).strftime(date_format),
+            }
+            data["bins"].append(dict_data)
 
         return data
