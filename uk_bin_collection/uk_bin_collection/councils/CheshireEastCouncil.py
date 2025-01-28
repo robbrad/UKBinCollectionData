4ef382afcd2c942f04b72cf33e78d3657a3e4ab4
Robert Bradley <robbrad182@gmail.com>
2025-01-28 20:17:40 +0000

    fix: Update CheshireEastCouncil.py

--- uk_bin_collection/uk_bin_collection/councils/CheshireEastCouncil.py
+++ uk_bin_collection/uk_bin_collection/councils/CheshireEastCouncil.py
@@ -10,6 +10,7 @@ class CouncilClass(AbstractGetBinDataClass):
     """
     A class to fetch and parse bin collection data for Cheshire East Council.
     """
+    
     def parse_data(self, page: Any, **kwargs: Any) -> Dict[str, Any]:
         soup = BeautifulSoup(page.text, features="html.parser")
 
