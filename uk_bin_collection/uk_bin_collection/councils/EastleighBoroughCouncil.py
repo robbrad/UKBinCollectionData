d9011571c08aff41d26cda9a17906145184f09d6
David Amor <david.amor@gmail.com>
2025-05-06 17:36:49 +0100

    Revert "fix: Eastleigh cloudflare fix"
    This reverts commit 2d486e80b64bf77d98ee13c4fa51715674225489.

--- uk_bin_collection/uk_bin_collection/councils/EastleighBoroughCouncil.py
+++ uk_bin_collection/uk_bin_collection/councils/EastleighBoroughCouncil.py
@@ -1,8 +1,4 @@
 from bs4 import BeautifulSoup
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import Select, WebDriverWait
-
 from uk_bin_collection.uk_bin_collection.common import *
 from uk_bin_collection.uk_bin_collection.get_bin_data import AbstractGetBinDataClass
 
@@ -16,69 +12,59 @@ class CouncilClass(AbstractGetBinDataClass):
     """
 
     def parse_data(self, page: str, **kwargs) -> dict:
-        try:
-            uprn = kwargs.get("uprn")
-            # Check the UPRN is valid
-            check_uprn(uprn)
-            headless = kwargs.get("headless")
-            web_driver = kwargs.get("web_driver")
-            url = f"https://www.eastleigh.gov.uk/waste-bins-and-recycling/collection-dates/your-waste-bin-and-recycling-collections?uprn={uprn}"
-            driver = create_webdriver(web_driver, headless, None, __name__)
-            driver.get(url)
+        uprn = kwargs.get("uprn")
+        # Check the UPRN is valid
+        check_uprn(uprn)
 
-            wait = WebDriverWait(driver, 10)
-            bin_content = wait.until(
-                EC.presence_of_element_located((By.CLASS_NAME, "dl-horizontal"))
-            )
+        # Request URL
+        url = f"https://www.eastleigh.gov.uk/waste-bins-and-recycling/collection-dates/your-waste-bin-and-recycling-collections?uprn={uprn}"
 
-            # Make a BS4 object from driver's page source
-            soup = BeautifulSoup(driver.page_source, features="html.parser")
+        # Make Request
+        requests.packages.urllib3.disable_warnings()
+        page = requests.get(url)
 
-            # Data to return
-            data = {"bins": []}
+        # Make a BS4 object
+        soup = BeautifulSoup(page.text, features="html.parser")
+        soup.prettify()
 
-            # Valid bin types
-            binTypes = [
-                "Household Waste Bin",
-                "Recycling Bin",
-                "Food Waste Bin",
-                "Glass Box and Batteries",
-                "Garden Waste Bin",
-            ]
+        # Data to return
+        data = {"bins": []}
 
-            # Value to create dict for DL values
-            keys, values = [], []
+        # Valid bin types
+        binTypes = [
+            "Household Waste Bin",
+            "Recycling Bin",
+            "Food Waste Bin",
+            "Glass Box and Batteries",
+            "Garden Waste Bin",
+        ]
 
-            # Loop though DT and DD for DL containing bins
-            dl = soup.find("dl", {"class": "dl-horizontal"})
-            for dt in dl.find_all("dt"):
-                keys.append(dt.text.strip())
-            for dd in dl.find_all("dd"):
-                values.append(dd.text.strip())
+        # Value to create dict for DL values
+        keys, values = [], []
 
-            # Create dict for bin name and string dates
-            binDict = dict(zip(keys, values))
+        # Loop though DT and DD for DL containing bins
+        dl = soup.find("dl", {"class": "dl-horizontal"})
+        for dt in dl.find_all("dt"):
+            keys.append(dt.text.strip())
+        for dd in dl.find_all("dd"):
+            values.append(dd.text.strip())
 
-            # Process dict for valid bin types
-            for bin in list(binDict):
-                if bin in binTypes:
-                    if not binDict[bin].startswith("You haven't yet signed up for"):
-                        # Convert date
-                        date = datetime.strptime(binDict[bin], "%a, %d %b %Y")
+        # Create dict for bin name and string dates
+        binDict = dict(zip(keys, values))
 
-                        # Set bin data
-                        dict_data = {
-                            "type": bin,
-                            "collectionDate": date.strftime(date_format),
-                        }
-                        data["bins"].append(dict_data)
+        # Process dict for valid bin types
+        for bin in list(binDict):
+            if bin in binTypes:
+                if not binDict[bin].startswith("You haven't yet signed up for"):
+                    # Convert date
+                    date = datetime.strptime(binDict[bin], "%a, %d %b %Y")
 
-            # Return bin data
-            return data
+                    # Set bin data
+                    dict_data = {
+                        "type": bin,
+                        "collectionDate": date.strftime(date_format),
+                    }
+                    data["bins"].append(dict_data)
 
-        except Exception as e:
-            print(f"Error fetching/parsing data: {str(e)}")
-            return {"bins": [{"type": "Error", "collectionDate": "2024-01-01"}]}
-        finally:
-            if "driver" in locals():
-                driver.quit()
+        # Return bin data
+        return data
