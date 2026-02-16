8e91639ac9777173ade43360e0d1634c11ae289d
TalhaMangarah <talha@talhamangarah.com>
2026-02-16 02:10:38 +0000

    fix: NewhamCouncil - disable SSL verification to resolve certificate verification errors

--- uk_bin_collection/uk_bin_collection/councils/NewhamCouncil.py
+++ uk_bin_collection/uk_bin_collection/councils/NewhamCouncil.py
@@ -19,7 +19,7 @@ class CouncilClass(AbstractGetBinDataClass):
             raise ValueError(f"Error getting identifier: {str(e)}")
 
         # Make a BS4 object
-        page = requests.get(url)
+        page = requests.get(url, verify=False)
         soup = BeautifulSoup(page.text, "html.parser")
         soup.prettify
 
