48913a49a3164848b6ea6e54158d666e15e3a6c6
InertiaUK <mark@keepcomputing.co.uk>
2026-04-07 09:03:43 +0100

    fix: SwaleBoroughCouncil - add user_agent to webdriver

--- uk_bin_collection/uk_bin_collection/councils/SwaleBoroughCouncil.py
+++ uk_bin_collection/uk_bin_collection/councils/SwaleBoroughCouncil.py
@@ -42,7 +42,8 @@ class CouncilClass(AbstractGetBinDataClass):
             council_url = "https://swale.gov.uk/bins-littering-and-the-environment/bins/check-your-bin-day"
 
             # Create Selenium webdriver
-            driver = create_webdriver(web_driver, headless, None, __name__)
+            user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36"
+            driver = create_webdriver(web_driver, headless, user_agent, __name__)
             driver.get(council_url)
 
             # Wait for the postcode field to appear then populate it
