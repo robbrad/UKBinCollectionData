674a3da210fd9901fa4fd4830d110b83aeee6d3f
InertiaUK <mark@keepcomputing.co.uk>
2026-04-07 10:38:16 +0100

    fix: add timeout and raise_for_status to request

--- uk_bin_collection/uk_bin_collection/councils/BlabyDistrictCouncil.py
+++ uk_bin_collection/uk_bin_collection/councils/BlabyDistrictCouncil.py
@@ -30,7 +30,8 @@ class CouncilClass(AbstractGetBinDataClass):
         session.headers.update({
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36"
         })
-        response = session.get(URI, verify=False)
+        response = session.get(URI, verify=False, timeout=30)
+        response.raise_for_status()
 
         # Parse the HTML
         soup = BeautifulSoup(response.content, "html.parser")
